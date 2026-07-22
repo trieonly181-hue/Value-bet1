3add5036c25706d7a49b40de8148d87acc3f2a3d
trieonly181-hue <trieonly181@gmail.com>
2026-07-23 00:42:45 +0300

    Create CheckoutBar.tsx

---
+++ components/CheckoutBar.tsx
@@ -0,0 +1,12 @@
+export default function CheckoutBar() {
+  return (
+    <div className="checkout">
+      <div>
+        <strong>Ready to order?</strong>
+        <br />
+        <span>Connect this button to Telegram checkout or WhatsApp order flow.</span>
+      </div>
+      <a className="btn primary" href="#">Checkout</a>
+    </div>
+  );
+}
